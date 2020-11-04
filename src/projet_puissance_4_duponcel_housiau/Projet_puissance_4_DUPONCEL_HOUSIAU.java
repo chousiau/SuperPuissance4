bee50f19cf38245d9d1bcbe64563535efc15adb6
Charl <Charl@LAPTOP-BT2ALE2H.home>
2020-11-04 10:38:53 +0100

    Grandes avancées sur la fonction grille

--- src/projet_puissance_4_duponcel_housiau/Projet_puissance_4_DUPONCEL_HOUSIAU.java
+++ src/projet_puissance_4_duponcel_housiau/Projet_puissance_4_DUPONCEL_HOUSIAU.java
@@ -21,21 +21,47 @@ public class Projet_puissance_4_DUPONCEL_HOUSIAU {
         Joueur Joueur1 = new Joueur("\u001B[31mO\u001B[0m");
         Joueur Joueur2 = new Joueur("\u001B[34mO\u001B[0m");
         Grille uneGrille = new Grille();
-        uneGrille.ajouterJetonDansColonne(unJeton, 7);
-        uneGrille.ajouterJetonDansColonne(unJeton, 6);
-        uneGrille.ajouterJetonDansColonne(unJeton, 5);
-
-
         
+
+        uneGrille.ajouterJetonDansColonne(unJeton2, 2);
         uneGrille.ajouterJetonDansColonne(unJeton, 2);
-        uneGrille.ajouterJetonDansColonne(unJeton, 2);
-        uneGrille.ajouterJetonDansColonne(unJeton, 2);
-        uneGrille.ajouterJetonDansColonne(unJeton, 2);
-        uneGrille.ajouterJetonDansColonne(unJeton, 2);
+        uneGrille.ajouterJetonDansColonne(unJeton2, 2);
         uneGrille.ajouterJetonDansColonne(unJeton2, 2);
         uneGrille.ajouterJetonDansColonne(unJeton, 2);
         
         uneGrille.ajouterJetonDansColonne(unJeton, 1);
+        uneGrille.ajouterJetonDansColonne(unJeton, 1);
+        uneGrille.ajouterJetonDansColonne(unJeton2, 1);
+        uneGrille.ajouterJetonDansColonne(unJeton, 1);
+        
+        uneGrille.ajouterJetonDansColonne(unJeton, 3);
+        uneGrille.ajouterJetonDansColonne(unJeton, 3);
+        
+        uneGrille.ajouterJetonDansColonne(unJeton, 4);
+        uneGrille.ajouterJetonDansColonne(unJeton2, 4);
+        uneGrille.ajouterJetonDansColonne(unJeton2, 4);
+        uneGrille.ajouterJetonDansColonne(unJeton2, 4);
+        uneGrille.ajouterJetonDansColonne(unJeton, 4);
+        uneGrille.ajouterJetonDansColonne(unJeton, 4);
+        
+        uneGrille.ajouterJetonDansColonne(unJeton2, 5);
+        uneGrille.ajouterJetonDansColonne(unJeton2, 5);
+        uneGrille.ajouterJetonDansColonne(unJeton, 5);
+        uneGrille.ajouterJetonDansColonne(unJeton2, 5);
+        uneGrille.ajouterJetonDansColonne(unJeton, 5);
+        uneGrille.ajouterJetonDansColonne(unJeton, 5);
+        
+        uneGrille.ajouterJetonDansColonne(unJeton2, 6);
+        uneGrille.ajouterJetonDansColonne(unJeton2, 6);
+        uneGrille.ajouterJetonDansColonne(unJeton, 6);
+        uneGrille.ajouterJetonDansColonne(unJeton, 6);
+        uneGrille.ajouterJetonDansColonne(unJeton, 6);
+        
+        
+        uneGrille.ajouterJetonDansColonne(unJeton2, 7);
+        uneGrille.ajouterJetonDansColonne(unJeton, 7);
+        uneGrille.ajouterJetonDansColonne(unJeton, 7);
+        
         System.out.println(uneGrille.etreGagnantePourJoueur(Joueur1));
         System.out.println(uneGrille.etreGagnantePourJoueur(Joueur2));
         
